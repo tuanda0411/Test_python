a0e1eac1987e095a2460e52458f0707cabc85bfe
Tuan Dang <danganhtuan114@gmail.com>
2020-09-04 20:52:42 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,24 @@
+1. Viết một chương trình có 2 chữ số, X, Y nhận giá trị từ đầu vào và tạo ra một mảng 2 chiều. Giá trị phần tử trong hàng thứ i và cột thứ j của mảng phải là i*j.
+Lưu ý: i=0,1,…,X-1; j=0,1,…,Y-1.
+Ví dụ: Giá trị X, Y nhập vào là 3,5 thì đầu ra là: [[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]]
+3. Viết một chương trình chấp nhận đầu vào là một câu, đếm số chữ cái và chữ số trong câu đó. Giả sử đầu vào sau được cấp cho chương trình: hello world! 123
+Thì đầu ra sẽ là: 
+Số chữ cái là: 10
+Số chữ số là: 3
+4. Write a program to produce Fibonacci series in Python
+5. Dãy số Fibonacci được định nghĩa như sau: F0 = 0, F1 = 1, F2 = 1, Fn = F(n-1) + F(n-2) với n >= 2. Ví dụ: 0, 1, 1, 2, 3, 5, 8, ... Hãy viết chương trình tìm n số Fibonacci đầu tiên.
+6. Viết chương trình liệt kê tất cả số nguyên tố có 5 chữ số trong Python.
+7. Viết chương trình phân tích số nguyên n thành các thừa số nguyên tố trong Python
+
+Q1. What is the difference between list and tuples in Python?
+Q2. What are the key features of Python?
+Q3. What type of language is python?
+Q4. How is Python an interpreted language?
+Q5. What is pep 8?
+Q6. How is memory managed in Python?
+Q7. What is name space in Python?
+Q8. What is PYTHON PATH?
+Q9. What are python modules?
+Q10. What are local variables and global variables in Python? Please give an exam
+Q11. What is __init__?
+Q12. What is self in Python?
